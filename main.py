700253da78fb2686e3b7ebbd4229237eff16c7e5
Chao Huang <hchao8@gmail.com>
2014-07-19 21:22:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,77 +1,34 @@
 from google.appengine.ext import ndb
 from flask import Flask, request, session, redirect, url_for, abort, \
-# -*- coding: utf-8 -*-
-"""
-    MiniTwit
-    ~~~~~~~~
-
-    A microblogging application written with Flask and sqlite3.
-
-    :copyright: (c) 2014 by Armin Ronacher.
-    :license: BSD, see LICENSE for more details.
-"""
 
 import time
-from sqlite3 import dbapi2 as sqlite3
 from hashlib import md5
 from datetime import datetime
 from flask import Flask, request, session, url_for, redirect, \
      render_template, abort, g, flash, _app_ctx_stack
 from werkzeug import check_password_hash, generate_password_hash
 
-
-# configuration
-DATABASE = '/tmp/minitwit.db'
-PER_PAGE = 30
-DEBUG = True
-SECRET_KEY = 'development key'
-
 # create our little application :)
 app = Flask(__name__)
 app.config.from_object(__name__)
-app.config.from_envvar('MINITWIT_SETTINGS', silent=True)
+app.config.from_envvar('FLASK_SETTINGS', silent=True)
+
+# Set up schemes
+class User(ndb.Model):
+    username = ndb.StringProperty(required=True)
+    email = ndb.StringProperty(required=True)
+    pw_hash = ndb.StringProperty(required=True)
+    following = ndb.ListProperty(ndb.Key)
+    start_date = ndb.DateTimeProperty(auto_now_add=True)
+    
+class Message(ndb.Model):
+    author = ndb.ReferenceProperty(User)
+    text = ndb.TextProperty(required=True)
+    pub_date = ndb.DateTimeProperty(auto_now_add=True)
 
 
-def get_db():
-    """Opens a new database connection if there is none yet for the
-    current application context.
-    """
-    top = _app_ctx_stack.top
-    if not hasattr(top, 'sqlite_db'):
-        top.sqlite_db = sqlite3.connect(app.config['DATABASE'])
-        top.sqlite_db.row_factory = sqlite3.Row
-    return top.sqlite_db
 
 
-@app.teardown_appcontext
-def close_database(exception):
-    """Closes the database again at the end of the request."""
-    top = _app_ctx_stack.top
-    if hasattr(top, 'sqlite_db'):
-        top.sqlite_db.close()
-
-
-def init_db():
-    """Creates the database tables."""
-    with app.app_context():
-        db = get_db()
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
-
-
-def query_db(query, args=(), one=False):
-    """Queries the database and returns a list of dictionaries."""
-    cur = get_db().execute(query, args)
-    rv = cur.fetchall()
-    return (rv[0] if rv else None) if one else rv
-
-
-def get_user_id(username):
-    """Convenience method to look up the id for a username."""
-    rv = query_db('select user_id from user where username = ?',
-                  [username], one=True)
-    return rv[0] if rv else None
 
 
 def format_datetime(timestamp):
